08608391963176a729ae3218d2402e51a2d3d73b
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-11-12 10:15:05 +0100

    5
    Co-authored-by: jjak0b
    Co-authored-by: iacop

--- src/bustapaga/buca01/BustaPaga.java
+++ src/bustapaga/buca01/BustaPaga.java
@@ -1,16 +1,16 @@
 package bustapaga.buca01;
 
 public class BustaPaga {
-    private final double stip;
+    private final double stipendio;
 
-    public BustaPaga(final double stpnd) {
-        this.stip = stpnd;
+    public BustaPaga(final double addValue) {
+        this.stipendio = addValue;
     }
 
     public double getNetto() {
-        final double pst = Math.max(Math.min(stip, 20000.0) - 5000, 0.0);
-        final double sst = Math.max(Math.min(stip, 40000) - 20000, 0.0);
-        final double tst = Math.max(stip - 40000, 0.0);
-        return stip - (pst * 0.1 + sst * 0.2 + tst * 0.4);
+        final double primoScaglione = Math.max(Math.min(stipendio, 20000.0) - 5000, 0.0);
+        final double secondoScaglione = Math.max(Math.min(stipendio, 40000) - 20000, 0.0);
+        final double terzoScaglione = Math.max(stipendio - 40000, 0.0);
+        return stipendio - (primoScaglione * 0.1 + secondoScaglione * 0.2 + terzoScaglione * 0.4);
     }
 }
